5560e41809c44836caee585c9350c73b6099d2e6
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2020-01-06 20:35:52 -0800

    Edited HouseInformation html and Java as suggested

--- src/main/java/website/fgto/royals_of_britain/view/houses/HouseInformationPage.java
+++ src/main/java/website/fgto/royals_of_britain/view/houses/HouseInformationPage.java
@@ -2,7 +2,15 @@ package website.fgto.royals_of_britain.view.houses;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import website.fgto.royals_of_britain.service.HousesService;
+import website.fgto.royals_of_britain.service.KingsService;
 import website.fgto.royals_of_britain.view.Apology;
 import website.fgto.royals_of_britain.view.Footer;
 import website.fgto.royals_of_britain.view.houses.intros.*;
@@ -54,4 +62,78 @@ public class HouseInformationPage extends WebPage {
         add(new Apology("introPanel"));
     }
   }
+
+  package website.fgto.royals_of_britain.view.houses;
+
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import website.fgto.royals_of_britain.service.HousesService;
+
+  public class KingsService extends Panel {
+    private static final long serialVersionUID = 1L;
+
+    @SpringBean
+    KingsService kingsService;
+
+    public KingsService(String id) {
+      super(id);
+
+      add(buildKingsServiceLinks());
+    }
+
+    private RepeatingView buildKingsServiceLinks() {
+      RepeatingView links = new RepeatingView("dropdownLinks");
+
+      kingsService
+          .findAll()
+          .forEach(
+              (house) -> links.add(buildKingsServiceItem(links, house.getName()))
+          );
+
+      return links;
+    }
+
+    private Link buildKingsServiceItem(RepeatingView parent, String KingsService) {
+      BookmarkablePageLink link =
+          new BookmarkablePageLink<Void>(parent.newChildId(), HouseInformationPage.class, buildPageParameters(KingsService));
+
+      link.setBody(new Model<>(capitalizedKingsService(KingsService));
+
+      return link;
+    }
+
+    private PageParameters buildPageParameters(String KingsService) {
+      PageParameters pageParameters = new PageParameters();
+      pageParameters.set("KingsService", KingsService);
+      return pageParameters;
+    }
+
+    private String capitalizedKingsService(String KingsService) {
+      if(KingsService.isEmpty()) {
+        return KingsService;
+      } else {
+        return KingsService.substring(0, 1).toUpperCase() + KingsService.substring(1).toLowerCase();
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
