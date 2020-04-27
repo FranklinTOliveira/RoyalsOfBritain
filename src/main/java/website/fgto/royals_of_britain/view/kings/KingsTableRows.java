abb0b626d256edc0eea14a3b38c1162483cd5145
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2020-04-27 15:24:38 -0700

    Rearranged the HTML and Java for KingsTableRows

--- src/main/java/website/fgto/royals_of_britain/view/kings/KingsTableRows.java
+++ src/main/java/website/fgto/royals_of_britain/view/kings/KingsTableRows.java
@@ -15,45 +15,33 @@ public class KingsTableRows extends Panel {
 
   public KingsTableRows(String id, String houseName) {
     super(id);
+
     add(buildRows(houseName));
   }
 
   private RepeatingView buildRows(String houseName) {
-    RepeatingView rows = new RepeatingView("heading");
-    if (houseName=!"") {
-      .findByHouse_Name(houseName)
-          .forEach(
-              (king) -> {
-                WebMarkupContainer heading = new WebMarkupContainer(rows.newChildId());
-
-                heading.add(new Label("nameHeading"));
-                heading.add(new Label("birthHeading"));
-                heading.add(new Label("deathHeading"));
-
-
-                rows.add(heading);
-              }
-          )}
-    else {
-      //add whitespace
-    }
-  }
-
-  private RepeatingView buildRows(String houseName) {
-    RepeatingView rows = new RepeatingView("kingRow");
+    RepeatingView rows = new RepeatingView("tableTemplate");
     kingsService
-      .findByHouse_Name(houseName)
-      .forEach(
-          (king) -> {
-            WebMarkupContainer row = new WebMarkupContainer(rows.newChildId());
+    if (rows.equals(houseName)){
+            .findByHouse_Name();
+            .forEach(
+            (king) -> {
+              WebMarkupContainer row = new WebMarkupContainer(rows.newChildId());
 
-            row.add(new Label("kingsName", new Model<>(king.getName())));
-            row.add(new Label("birthYear", new Model<>(king.getYearOfBirth())));
-            row.add(new Label("deathYear", new Model<>(king.getYearOfDeath())));
+              row.add(new Label("nameHeading"));
+              row.add(new Label("birthHeading"));
+              row.add(new Label("deathHeading"));
+              row.add(new Label("kingsName", new Model<>(king.getName())));
+              row.add(new Label("birthYear", new Model<>(king.getYearOfBirth())));
+              row.add(new Label("deathYear", new Model<>(king.getYearOfDeath())));
 
-            rows.add(row);
-          }
-      );
-    return rows;
+              rows.add(row);
+            }
+            );
+            return rows;
+        } else {
+        String.format("%-20s");
+
+        System.out.println(String.format("%-20s"));
+        }
   }
-}
