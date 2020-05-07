1818e6e8ddd716e2f7aa277ca577ea88fde1916e
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2020-05-07 16:08:00 -0700

    Revealed addition of the Bootstrap CSS

--- src/main/java/website/fgto/royals_of_britain/view/HomePage.java
+++ src/main/java/website/fgto/royals_of_britain/view/HomePage.java
@@ -3,15 +3,12 @@ package website.fgto.royals_of_britain.view;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+
 import org.apache.wicket.request.resource.CssResourceReference;
 import website.fgto.royals_of_britain.WicketApplication;
-import website.fgto.royals_of_britain.view.Apology;
-import website.fgto.royals_of_britain.view.Footer;
+
 import website.fgto.royals_of_britain.view.houses.HousesDropdown;
-import website.fgto.royals_of_britain.view.houses.intros.*;
-import website.fgto.royals_of_britain.view.kings.KingsTableRows;
+
 
 public class HomePage extends WebPage {
   private static final long serialVersionUID = 1L;
