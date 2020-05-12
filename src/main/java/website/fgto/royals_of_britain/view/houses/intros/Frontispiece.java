4bdaa900c5d30d22d4a3e960a82d8ec4dff74b93
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2020-05-12 15:00:14 -0700

    Wicket Image Resource Addition

--- src/main/java/website/fgto/royals_of_britain/view/houses/intros/Frontispiece.java
+++ src/main/java/website/fgto/royals_of_britain/view/houses/intros/Frontispiece.java
@@ -1,11 +1,22 @@
 package website.fgto.royals_of_britain.view.houses.intros;
 
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class Frontispiece extends Panel {
   private static final long serialVersionUID = 1L;
 
   public Frontispiece(String id) {
     super(id);
+  }
+
+  public static class FrontispieceImage extends Panel {
+    public FrontispieceImage(String id){
+      super(id);
+      PackageResourceReference resourceReference =
+          new PackageResourceReference(getClass(), "img/FlagsOfTheUnionJack.jpg");
+          add(new Image("frontispiece", resourceReference));
+    }
   }
 }
